e30c9fc1e2fa673c2dea117a99555ad5ab570159
Jose Gutierrez <jose.gutierrez.sagredo@gmail.com>
2020-11-11 15:13:07 +0100

    add expo update

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Update from 'expo-updates'
 export default function App() {
+  React.useEffect(() => {
+    Update.reloadAsync()
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
